e7fb686646b5aee54692b129eb93abb032ddbc3b
Seoki <pkoi5088@gmail.com>
2023-09-22 00:15:25 +0900

    SharedDB 구현

---
+++ Server/SharedDB/DataModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedDB
+{
+    [Table("Token")]
+    public class TokenDb
+    {
+        public int TokenDbId { get; set; }
+        public int AccountDbId { get; set; }
+        public int Token { get; set; }
+        public DateTime Expired { get; set; }
+    }
+
+    [Table("ServerInfo")]
+    public class ServerDb
+    {
+        public int ServerDbId {  get; set; }
+        public string Name {  get; set; }
+        public string IpAddress { get; set; }
+        public int Port { get; set; }
+        public int BusyScore { get; set; }
+    }
+}
